2a92caa4a615997c189fc4555f43f57a0ac266b3
nouralfarwie <nouralfarwie@gmail.com>
2024-04-16 17:37:52 +0300

    in this code give me the std_id null

--- lib/screens/call.dart
+++ lib/screens/call.dart
@@ -1,18 +1,63 @@
 import 'package:flutter/material.dart';
-import 'package:letsgohome/component/textformfield_ns.dart';
+import 'package:http/http.dart' as http;
+import 'package:shared_preferences/shared_preferences.dart';
+import 'dart:convert';
+
+import '../component/textformfield_ns.dart';
+
 class CallPage extends StatefulWidget {
-  const CallPage({super.key});
+  String? std_id ;
+   CallPage({Key? key, this.std_id}) : super(key: key);
 
   @override
   State<CallPage> createState() => _CallPageState();
 }
 
 class _CallPageState extends State<CallPage> {
+  TextEditingController depcontroll = TextEditingController();
+  TextEditingController attachController = TextEditingController();
+  TextEditingController reason = TextEditingController();
+
+  Future<void> sendData() async {
+    print(widget.std_id);
+    // API endpoint URL
+    var url = Uri.parse('https://present-ksa.com/api/earlycall');
+    final Future<SharedPreferences> _prefs = SharedPreferences.getInstance();
+    final SharedPreferences prefs = await _prefs;
+    String token = prefs.getString('token')!;
+    print('widget.std_id: ${widget.std_id}');
+    // Request body
+    var requestBody = {
+      'id': widget.std_id,
+      'leaving_time': depcontroll.text,
+      'reason': reason.text ,
+      'document': attachController.text,
+    };
+    print('requestBody: $requestBody');
+
+    // Send POST request
+    var response = await http.post(url, body: requestBody,
+      headers: <String, String>{
+        // 'Content-Type': 'application/json',
+        'Authorization': 'Bearer $token',
+      },
+    );
+    print(requestBody);
+    print(response.statusCode);
+    // Check if request was successful
+    if (response.statusCode == 200) {
+      // Handle success
+      print('Request successful');
+    } else {
+      // Handle error
+      print('Request failed with status: ${response.statusCode}');
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: Column(
-
         children: [
           Stack(
             children: [
@@ -31,11 +76,7 @@ class _CallPageState extends State<CallPage> {
                 top: 30,
                 left: 20,
                 child: IconButton(
-                  onPressed: () {
-                    // Navigator.of(context).push(
-                    // MaterialPageRoute(builder: (context) => HomePage()),
-                    // );
-                  },
+                  onPressed: () {},
                   icon: Icon(Icons.arrow_back),
                 ),
               ),
@@ -66,11 +107,7 @@ class _CallPageState extends State<CallPage> {
                   ),
                   child: Center(
                     child: IconButton(
-                      onPressed: () {
-                        // Navigator.of(context).push(
-                        // MaterialPageRoute(builder: (context) => HomePage()),
-                        // );
-                      },
+                      onPressed: () {},
                       icon: Icon(
                         Icons.add_alert,
                         size: 23,
@@ -82,10 +119,8 @@ class _CallPageState extends State<CallPage> {
             ],
           ),
           Padding(
-            padding: const EdgeInsets.only(left: 10,top: 10,),
+            padding: const EdgeInsets.only(left: 10, top: 10),
             child: Container(
-
-
               margin: EdgeInsets.only(left: 10),
               padding: EdgeInsets.all(10),
               height: 44,
@@ -105,13 +140,11 @@ class _CallPageState extends State<CallPage> {
                       fontWeight: FontWeight.bold,
                     ),
                   ),
-
                 ],
               ),
             ),
           ),
-          SizedBox(height: 20,),
-          
+          SizedBox(height: 20),
           Expanded(
             child: ListView(
               children: [
@@ -119,33 +152,36 @@ class _CallPageState extends State<CallPage> {
                   margin: EdgeInsets.all(10),
                   width: 300,
                   height: 500,
-                  decoration: BoxDecoration(borderRadius: BorderRadius.all(Radius.circular(30),
-                  ),
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(30),
+                    ),
                     gradient: LinearGradient(
                       colors: [
-                        Color.fromRGBO(76, 149, 91, 0.5), // لون أخضر فاتح
-                        Color.fromRGBO(28, 104, 15, 1), // لون أخضر داكن
+                        Color.fromRGBO(76, 149, 91, 0.5),
+                        Color.fromRGBO(28, 104, 15, 1),
                       ],
                       begin: Alignment.bottomRight,
                       end: Alignment.centerLeft,
-                    ),),
+                    ),
+                  ),
                   child: ListView(
                     children: [
-                      SizedBox(height: 10,),
+                      SizedBox(height: 10),
                       Padding(
                         padding: const EdgeInsets.all(10.0),
                         child: Container(
-                          //padding: EdgeInsets.only(left: 8,top: 8),
-                          width: 250, // Adjust the width as needed
+                          width: 250,
                           height: 100,
                           margin: EdgeInsets.only(top: 8),
                           child: TextFormField(
+                            controller: reason,
                             maxLines: 10,
                             decoration: InputDecoration(
                               prefixIcon: Icon(Icons.note_alt_rounded, color: Colors.black),
                               labelText: "Cause leave",
                               filled: true,
-                              fillColor: Colors.white, // Set background color to white
+                              fillColor: Colors.white,
                               enabledBorder: OutlineInputBorder(
                                 borderRadius: BorderRadius.circular(10.0),
                                 borderSide: BorderSide(color: Colors.grey, width: 2.0),
@@ -168,21 +204,28 @@ class _CallPageState extends State<CallPage> {
                           ),
                         ),
                       ),
-                      SizedBox(height: 10,),
+                      SizedBox(height: 10),
                       Padding(
                         padding: const EdgeInsets.all(10.0),
-                        child: CustomTextField(prefixIcon: Icons.alarm,labelText: "Depature hour"),
+                        child: CustomTextField(
+                          prefixIcon: Icons.alarm,
+                          labelText: "Depature hour",
+                          controller: depcontroll,
+                        ),
                       ),
-                      SizedBox(height: 10,),
+                      SizedBox(height: 10),
                       Padding(
                         padding: const EdgeInsets.all(10.0),
-                        child: CustomTextField(prefixIcon: Icons.attach_file,labelText: "Attach file/photo",),
+                        child: CustomTextField(
+                          prefixIcon: Icons.attach_file,
+                          labelText: "Attach file/photo",
+                          controller: attachController,
+                        ),
                       ),
                       Padding(
                         padding: const EdgeInsets.all(30.0),
                         child: Container(
-
-                          margin: EdgeInsets.only(top: 20,bottom: 20),
+                          margin: EdgeInsets.only(top: 20, bottom: 20),
                           width: 100,
                           height: 50,
                           decoration: BoxDecoration(
@@ -195,18 +238,19 @@ class _CallPageState extends State<CallPage> {
                           ),
                           child: OutlinedButton(
                             onPressed: () {
-                              // Navigator.of(context).push(MaterialPageRoute(builder: (context)=> HomePage()));
-
+                              print("resppppons");
+                              sendData();
                             },
                             style: OutlinedButton.styleFrom(
-                              primary:  Colors.white,
+                              primary: Colors.white,
                               shape: RoundedRectangleBorder(
                                 borderRadius: BorderRadius.circular(8),
                               ),
                             ),
-                            child: Text("Save",style: TextStyle(
-                                fontSize: 20
-                                , fontWeight:FontWeight.bold, color: Color(0xFF5C955D)),),
+                            child: Text(
+                              "Save",
+                              style: TextStyle(fontSize: 20, fontWeight: FontWeight.bold, color: Color(0xFF5C955D)),
+                            ),
                           ),
                         ),
                       ),
@@ -216,11 +260,8 @@ class _CallPageState extends State<CallPage> {
               ],
             ),
           ),
-
         ],
-      )
-
-    ,
+      ),
     );
   }
 }
